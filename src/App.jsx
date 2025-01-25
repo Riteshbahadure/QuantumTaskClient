216f25d0874e4ed883bea8e78ba879a0b90e9db2
Rutuja Bahadure <bahadurerutu558@gamil.com>
2025-01-25 16:04:42 +0530

    app.jsx changed

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,10 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify'
 
 import 'react-toastify/dist/ReactToastify.css'
-import UserRegister from './pages/userRegister'
+// import UserRegister from './pages/userRegister'
 import UserLogin from './pages/UserLogin'
 import Home from './pages/Home'
+import UserRegister from './pages/UserRegister'
 // import Test from './pages/Test'
 
 const App = () => {
@@ -13,11 +14,11 @@ const App = () => {
     <BrowserRouter>
       <ToastContainer />
       <Routes>
- 
+
         {/* <Route path='/test' element={<Test />} /> */}
         <Route path='/' element={<UserRegister />} />
-        <Route path='/home' element={<Home />} />      
-        <Route path='/login' element={<UserLogin />} />      
+        <Route path='/home' element={<Home />} />
+        <Route path='/login' element={<UserLogin />} />
         <Route path='*' element={<h1>Page Not Found</h1>} />
       </Routes>
     </BrowserRouter>
